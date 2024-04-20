3182b9eb010ee2e5a0c4de845a2c9ea2d56ab567
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2024-04-20 19:02:29 +0200

    started building meetups app

--- react-review/src/App.js
+++ react-review/src/App.js
@@ -1,18 +1,5 @@
-import Backdrop from "./components/Backdrop";
-import Todo from "./components/Todo";
-import Modal from "./components/modal";
-
 function App() {
-  return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo text="learn react" />
-      <Todo text="master react" />
-      <Todo text="explore the full react course" />
-      <Modal />
-      <Backdrop />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
